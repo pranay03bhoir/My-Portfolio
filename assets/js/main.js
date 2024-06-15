d7ad7fabfa3283c9834e43fef7f8223a610918d8
Pranay Bhoir <147631196+pranay03bhoir@users.noreply.github.com>
2024-06-16 00:15:45 +0530

    My portfolio files

---
+++ assets/js/main.js
@@ -0,0 +1,54 @@
+function scrollToElement(elementSelector, instance=0){
+    const elements = document.querySelectorAll(elementSelector);
+
+    if(elements.length>instance){
+        elements[instance].scrollIntoView({ behavior: "smooth" });
+    }
+}
+const link1 = document.getElementById("link1");
+const link2 = document.getElementById("link2");
+const link3 = document.getElementById("link3");
+const link4 = document.getElementById("link4");
+const link5 = document.getElementById("link5");
+
+
+link1.addEventListener("click", () => {
+  scrollToElement(".abtcnt1");
+});
+
+link2.addEventListener("click", () => {
+  scrollToElement(".frontendtech");
+});
+
+link3.addEventListener("click", () => {
+  scrollToElement(".heading");
+});
+link4.addEventListener("click", () => {
+  scrollToElement(".contact");
+});
+link5.addEventListener("click", () => {
+  scrollToElement(".header");
+});
+
+
+function HandleBar(){
+    let bar = document.getElementById('bars');
+    let navbar = document.getElementById('navbar')
+    let closenav = document.getElementById("closenav");
+    if(bar.getAttribute('id') == 'bars'){
+        navbar.style.display='block'
+        bar.style.display='none'
+       closenav.style.display = "block";
+
+    }
+}
+function CloseBar(){
+    let closenav = document.getElementById('closenav');
+    let navbar = document.getElementById("navbar");
+    let bar = document.getElementById("bars");
+     if (closenav.getAttribute("id") == "closenav") {
+       navbar.style.display = "none";
+       closenav.style.display = "none";
+       bar.style.display = "block";
+     }
+}
