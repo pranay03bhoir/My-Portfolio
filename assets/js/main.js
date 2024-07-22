d41c8f23a9cf8a1c399be50b97592de4ce95a2f9
Pranay Bhoir <147631196+pranay03bhoir@users.noreply.github.com>
2024-07-22 15:21:01 +0530

    New commit (Updated responsive navbar)

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,12 +5,16 @@ function scrollToElement(elementSelector, instance=0){
         elements[instance].scrollIntoView({ behavior: "smooth" });
     }
 }
+const link = document.getElementById("link");
 const link1 = document.getElementById("link1");
 const link2 = document.getElementById("link2");
 const link3 = document.getElementById("link3");
 const link4 = document.getElementById("link4");
 const link5 = document.getElementById("link5");
 
+link.addEventListener("click", () => {
+  scrollToElement(".header");
+});
 
 link1.addEventListener("click", () => {
   scrollToElement(".abtcnt1");
